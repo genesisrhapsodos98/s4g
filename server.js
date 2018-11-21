ccdb9e59a8e62cece026f050ef42ba2b5494fcf1
Genesis <genesisrhapsodos98@gmail.com>
2018-11-21 20:18:19 +0700

    Set listened port to dynamic

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.get('/', function (req, res) {
   res.render('index');
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.post('/', function (req, res) {
+  res.render('index');
 })
+
+app.listen(process.env.PORT || 5000);

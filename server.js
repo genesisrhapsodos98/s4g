c71b50bb8c9ebe1d61bf39240c024c53a6e83ec1
Genesis <genesisrhapsodos98@gmail.com>
2018-11-23 00:57:24 +0700

    Routing, Login form

--- server.js
+++ server.js
@@ -4,13 +4,35 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  //res.send('Hello World!')
-  res.render('index');
+  res.render('index', { onLogin : "onLogin();"});
 })
 
 app.post('/', function (req, res) {
-  res.render('index');
+
 })
+
+app.get('/products', function (req, res) {
+  //res.send('Hello World!')
+  res.render('products');
+})
+
+app.get('/contact', function (req, res) {
+  //res.send('Hello World!')
+  res.render('contact');
+})
+
+app.get('/signup', function (req, res) {
+  //res.send('Hello World!')
+  res.render('signup');
+})
+
+app.get('/forgotpassword', function (req, res) {
+  res.render('forgotpassword');
+})
+
+/*app.post('/', function (req, res) {
+  res.render('index');
+})*/
 
 app.listen(process.env.PORT || 5000, function() {
   console.log('Listening to Heroku\'s dynamic port and port 5000');

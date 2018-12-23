8534bbf7325585bb1d85fef51f9a4e776d247ff6
Genesis <genesisrhapsodos98@gmail.com>
2018-12-23 12:11:13 +0700

    Commented on some js files

--- routes/category.js
+++ routes/category.js
@@ -1,26 +1,29 @@
+// Import dependencies
 var express = require('express');
 var router = express.Router();
 
+// Config router
 router.use(express.static(__dirname + '../public'));
 
+// Routing
 router.get('/cat1', function(req, res) {
   res.render('category/cat1');
-})
+});
 
 router.get('/cat2', function(req, res) {
   res.render('category/cat2');
-})
+});
 
 router.get('/cat3', function(req, res) {
   res.render('category/cat3');
-})
+});
 
 router.get('/cat4', function(req, res) {
   res.render('category/cat4');
-})
+});
 
 router.get('/cat5', function(req, res) {
   res.render('category/cat5');
-})
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
+// Import dependencies
 const express = require('express')
-const app = express()
+const app = express();
 var routes = require('./routes/index');
 var category = require('./routes/category');
 
+// Config router
 app.set('view engine', 'ejs');
 app.set('views', __dirname + "/views/pages");
-
 app.use(express.static(__dirname + '/public'));
 app.use('/', routes);
 app.use('/category', category);
 
+// Start server
 app.listen(process.env.PORT || 5000, function() {
   console.log('Listening to Heroku\'s dynamic port and port 5000');
 })
